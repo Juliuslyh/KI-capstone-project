dceb9741158e725516b2ce46d1e846061217dfc2
Yihao Li <juliuslyh@gmail.com>
2020-08-17 10:24:32 +0200

    modify the path to save as csv file

--- DataCollection/GenerateData.py
+++ DataCollection/GenerateData.py
@@ -4,13 +4,15 @@ import Absatz.Crawler_Absatz
 from Feiertage.feiertageCheck import feiertag
 from Bewertung.Crawler_Bewertung import BewertungCrawler
 from Bewertung.Paragraph_to_sentence import Para_to_Sentence
+import os
 
 if __name__ == '__main__':
     BewertungCrawler()
     Para_to_Sentence()
     feiertag().load_data()
     df = WeatherCrawler().df
-    df.to_csv('Weather.csv')
+    df.to_csv(os.path.abspath(os.path.dirname(os.getcwd())) + '/data/'
+              + 'Weather.csv')
     try:
         UrlCrawler().load_data()
     except IOError:
